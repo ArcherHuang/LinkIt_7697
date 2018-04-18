f63b17ceba5ed0537361e818a7e18e72454bcd58
ArcherHuang <mmosconii@gmail.com>
2018-04-18 22:39:44 +0800

    add MQTT_MCS_Ultrasonic

---
+++ Arduino/MQTT_MCS_Ultrasonic/MQTT_MCS_Ultrasonic.ino
@@ -0,0 +1,86 @@
+#include <LWiFi.h>
+#include <PubSubClient.h>
+
+#include <Ultrasonic.h>
+
+#define buzzerPIN 4
+Ultrasonic ultrasonic(2, 3);
+
+char ssid[] = "";
+char password[] = "";
+char mqtt_server[] = "mqtt.mcs.mediatek.com";
+char pub_topic[] = "mcs/deviceId/deviceKey/dataChnId";
+char client_Id[] = "linkit-7697";
+ 
+int status = WL_IDLE_STATUS;
+
+WiFiClient mtclient;     
+PubSubClient client(mtclient);
+
+// JSON
+String msgStr = "";
+char json[79];
+
+void setup() {
+    //Initialize serial and wait for port to open:
+    Serial.begin(9600);
+
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    pinMode(buzzerPIN, OUTPUT);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  msgStr = "";
+
+  int distance = ultrasonic.distanceRead();
+  char ultrasonic_distance[75];
+  sprintf(ultrasonic_distance, "%d", distance);
+  msgStr = msgStr + "{\"datapoints\": [{\"values\": {\"value\":" + ultrasonic_distance + "}, \"dataChnId\":\"Ultrasonic\"}]}";
+  msgStr.toCharArray(json, 90);
+  Serial.print("Publish message: ");
+  Serial.println(json);
+  Serial.println(msgStr);
+  client.publish(pub_topic, json);
+  delay(5000);
+}
+
+void setup_wifi() {                       //setup Wifi
+   // attempt to connect to Wifi network:
+   Serial.print("Attempting to connect to SSID: ");
+   Serial.println(ssid);
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.print(".");
+    }
+    randomSeed(micros());
+    Serial.println("Connected to wifi");
+}
+
+void reconnect() {  //reconnect MQTT
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = client_Id;
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish(pub_topic, client_Id);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
